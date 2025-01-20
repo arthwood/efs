ced18ae2f23acd128d7992ee9ccafcea17548027
Artur Bilski <admin@arturbilski.pl>
2025-01-20 17:21:50 +0100

    Import and use `expo-file-system/next`.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,12 @@
+import { Directory, File, Paths } from 'expo-file-system/next'
 import { Image, StyleSheet, Platform } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+
+const pdfDestination = new Directory(Paths.cache, 'pdfs')
 
 export default function HomeScreen() {
   return (
